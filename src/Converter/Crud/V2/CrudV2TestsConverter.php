9509e612bde4fc0d5d000265f54fbb7abd8a0092
Andreas Braun <git@alcaeus.org>
2021-04-16 08:39:23 +0200

    Extract operation converter for reusability

--- src/Converter/Crud/V2/CrudV2TestsConverter.php
+++ src/Converter/Crud/V2/CrudV2TestsConverter.php
@@ -2,6 +2,7 @@
 
 namespace App\Converter\Crud\V2;
 
+use App\Converter\Operation\LegacyOperationConverter;
 use App\Converter\TestItemConverterInterface;
 use Symfony\Component\Yaml\Reference\Anchor;
 use Symfony\Component\Yaml\Reference\Reference;
@@ -13,10 +14,7 @@ final class CrudV2TestsConverter implements TestItemConverterInterface
 {
     public function convert(string $fieldName, mixed $data): mixed
     {
-        $operations = array_map(
-            fn ($operation): array => $this->convertOperation($operation),
-            $data['operations'],
-        );
+        $operations = $this->convertOperations($data['operations']);
 
         if (isset($operation['failPoint'])) {
             array_unshift($operations, [
@@ -42,36 +40,6 @@ final class CrudV2TestsConverter implements TestItemConverterInterface
             ],
             fn ($data): bool => $data !== null,
         );
-    }
-
-    private function convertOperation(array $operation): array
-    {
-        if ($operation['name'] === 'bulkWrite') {
-            $operation['arguments']['requests'] = array_map(
-                fn ($request) => [
-                    $request['name'] => $request['arguments'],
-                ],
-                $operation['arguments']['requests'],
-            );
-        }
-
-        $unifiedOperation = [
-            'object' => new Reference($this->getOperationObjectName($operation['object'] ?? 'collection')),
-            // collectionOptions not supported, will cause errors to point out manual work
-            'collectionOptions' => $data['collectionOptions'] ?? null,
-            'name' => $operation['name'],
-            'arguments' => $operation['arguments'],
-            // error handled below
-            // result handled below
-        ];
-
-        if (isset($operation['error'])) {
-            $unifiedOperation['expectError'] = ['isError' => $operation['error']];
-        } elseif (isset($operation['result'])) {
-            $unifiedOperation['expectResult'] = $operation['result'];
-        }
-
-        return $unifiedOperation;
     }
 
     private function convertExpectedEvents(?array $expectations): ?array
@@ -113,8 +81,13 @@ final class CrudV2TestsConverter implements TestItemConverterInterface
         return isset($anchorName) ? new Anchor($anchorName, $result) : $result;
     }
 
-    private function getOperationObjectName(string $object): string
+    private function convertOperations(mixed $operations): array
     {
-        return $object === 'collection' ? 'collection0' : 'database0';
+        $converter = new LegacyOperationConverter();
+
+        return array_map(
+            fn($operation): array => $converter->convert('', $operation),
+            $operations,
+        );
     }
 }
---
+++ src/Converter/Operation/LegacyOperationConverter.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Converter\Operation;
+
+use App\Converter\TestItemConverterInterface;
+use Symfony\Component\Yaml\Reference\Reference;
+use function array_map;
+
+final class LegacyOperationConverter implements TestItemConverterInterface
+{
+    private const DEFAULT_OBJECT_NAME_MAPPINGS = [
+        'collection' => 'collection0',
+        'database' => 'database0',
+    ];
+
+    public function __construct(private array $objectNameMappings = self::DEFAULT_OBJECT_NAME_MAPPINGS) {}
+
+    public function convert(string $fieldName, mixed $data): ?array
+    {
+        if ($data === null) {
+            return null;
+        }
+
+        if ($data['name'] === 'bulkWrite') {
+            $data['arguments']['requests'] = array_map(
+                fn ($request) => [
+                    $request['name'] => $request['arguments'],
+                ],
+                $data['arguments']['requests'],
+            );
+        }
+
+        if (is_array($data['arguments']) && isset($data['arguments']['options'])) {
+            $options = $data['arguments']['options'];
+            unset($data['arguments']['options']);
+            $data['arguments'] = array_merge($data['arguments'], $options);
+        }
+
+        $unifiedOperation = [
+            'object' => new Reference($this->getOperationObjectName($data['object'] ?? 'collection')),
+            // collectionOptions not supported, will cause validation errors to point out manual work
+            'collectionOptions' => $data['collectionOptions'] ?? null,
+            'name' => $data['name'],
+            'arguments' => $data['arguments'],
+            // error handled below
+            // result handled below
+        ];
+
+        if (isset($data['error'])) {
+            $unifiedOperation['expectError'] = ['isError' => $data['error']];
+        } elseif (isset($data['result'])) {
+            $unifiedOperation['expectResult'] = $data['result'];
+        }
+
+        return $unifiedOperation;
+    }
+
+    private function getOperationObjectName(string $object): string
+    {
+        return $this->objectNameMappings[$object] ?? $object;
+    }
+}
