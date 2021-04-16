2635b23b71e491010f5d248e160653b4dcbd84cb
Andreas Braun <git@alcaeus.org>
2021-04-16 09:04:50 +0200

    Add missing default option for update command expectations

--- src/Converter/Crud/V2/CrudV2TestsConverter.php
+++ src/Converter/Crud/V2/CrudV2TestsConverter.php
@@ -12,6 +12,11 @@ use function array_unshift;
 
 final class CrudV2TestsConverter implements TestItemConverterInterface
 {
+    private const DEFAULT_UPDATE_CMD_OPTIONS = [
+        'multi' => ['$$unsetOrMatches' => false],
+        'upsert' => ['$$unsetOrMatches' => false],
+    ];
+
     public function convert(string $fieldName, mixed $data): mixed
     {
         $operations = $this->convertOperations($data['operations']);
@@ -93,7 +98,23 @@ final class CrudV2TestsConverter implements TestItemConverterInterface
 
     private function convertCommandExpectations(array $command): array
     {
+        $command = $this->addMissingUpdateCommandOptions($command);
         return $this->convertExpectedNullValues($command);
+    }
+
+    private function addMissingUpdateCommandOptions(array $command): array
+    {
+        $commandName = array_keys($command)[0];
+        if ($commandName !== 'update') {
+            return $command;
+        }
+
+        $command['updates'] = array_map(
+            fn (array $update): array => $update + self::DEFAULT_UPDATE_CMD_OPTIONS,
+            $command['updates'],
+        );
+
+        return $command;
     }
 
     private function convertExpectedNullValues(array $array): array
